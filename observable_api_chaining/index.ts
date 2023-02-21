c3ea2edf54bb9a1bf431d7060c7a7c2796f4d34b
Mervyn Manilall <mervyn15@outlook.com>
2023-02-21 23:00:12 +0400

    adding comments in chaining

--- observable_api_chaining/index.ts
+++ observable_api_chaining/index.ts
@@ -4,6 +4,10 @@ import { of, map, Observable, switchMap } from 'rxjs';
 import { errorContext } from 'rxjs/internal/util/errorContext';
 
 // calling api and chaining
+// in this first part we will be only doing the 2 calls to see if nothing is breaking
+
+// ------------- First part: doing the seperate calls
+// observable for 1st api call 
 const firstObservable$ = new Observable((observer) => {
   fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then((response) => response.json())
@@ -14,6 +18,7 @@ const firstObservable$ = new Observable((observer) => {
     .catch((error) => console.log('error'));
 });
 
+// observable for 2nd api call 
 const secondObserver$ = new Observable((observer) => {
   fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
     .then((response) => response.json())
@@ -24,6 +29,7 @@ const secondObserver$ = new Observable((observer) => {
     .catch((error) => console.log(error));
 });
 
+// subscriber for 1st call
 console.log("let's get started");
 firstObservable$.subscribe({
   next: (data) => console.log('result from 1st: ', data),
@@ -33,13 +39,16 @@ firstObservable$.subscribe({
 
 console.log("let's continue");
 
+// subscriber for 2nd call
 secondObserver$.subscribe({
   next: (data) => console.log(data),
   error: (error) => console.log('error'),
   complete: () => console.log('2nd done'),
 });
 
+// --------------- 2nd part: Chaining
 // chaining
+// in this second part we are doing the actual chaining
 const chainingObservable$ = firstObservable$.pipe(
   switchMap((data) => {
     return new Observable((observer) => {
